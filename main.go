8bff33d8c444298449510f568f140ce002a7b9dc
Oliver Bestmann <oliver.bestmann@googlemail.com>
2016-07-17 17:31:01 +0200

    Reorder code

--- main.go
+++ main.go
@@ -8,92 +8,13 @@ import (
 	"github.com/gorilla/websocket"
 	"io/ioutil"
 	"net/http"
-	"sync"
-	"time"
+	"github.com/oliverbestmann/go-websocket-publish/ws"
 )
-
-type Stream struct {
-	Id      string    `json:"id"`
-	Created time.Time `json:"created"`
-
-	hub *Hub
-}
-
-type Registrar struct {
-	lock    sync.RWMutex
-	streams map[string]*Stream
-}
-
-func NewRegistrar() *Registrar {
-	return &Registrar{
-		lock:    sync.RWMutex{},
-		streams: make(map[string]*Stream),
-	}
-}
-
-func (r *Registrar) GetExistingHub(streamId string) *Hub {
-	r.lock.RLock()
-	defer r.lock.RUnlock()
-
-	stream := r.streams[streamId]
-	if stream == nil {
-		return nil
-	}
-
-	return stream.hub
-}
-
-func (r *Registrar) GetOrCreateHub(streamId string) *Hub {
-	r.lock.Lock()
-	defer r.lock.Unlock()
-
-	stream, _ := r.streams[streamId]
-	if stream == nil {
-		stream = &Stream{
-			Id:      streamId,
-			Created: time.Now(),
-			hub:     NewHub(),
-		}
-
-		r.streams[streamId] = stream
-
-		go stream.hub.MainLoop()
-	}
-
-	return stream.hub
-}
-
-func (r *Registrar) GetStreams() []Stream {
-	r.lock.Lock()
-	defer r.lock.Unlock()
-
-	// get a copy of the streams id.
-	streams := make([]Stream, 0, len(r.streams))
-	for _, stream := range r.streams {
-		streams = append(streams, *stream)
-	}
-
-	return streams
-}
-
-func (r *Registrar) Close(streamId string) bool {
-	r.lock.Lock()
-	defer r.lock.Unlock()
-
-	if stream := r.streams[streamId]; stream != nil {
-		delete(r.streams, streamId)
-		stream.hub.RequestShutdown()
-		return true
-
-	} else {
-		return false
-	}
-}
 
 func main() {
 	router := mux.NewRouter()
 
-	registrar := NewRegistrar()
+	registrar := ws.NewRegistrar()
 
 	router.Path("/streams").Methods("GET").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		streams := registrar.GetStreams()
@@ -134,6 +55,8 @@ func main() {
 		vars := mux.Vars(req)
 		streamId := vars["stream"]
 
+		// TODO check for access rights and stuff
+
 		if !registrar.Close(streamId) {
 			w.WriteHeader(http.StatusNotFound)
 		}
@@ -144,7 +67,7 @@ func main() {
 			handlers.RecoveryHandler()(router))))
 }
 
-func handleClientWebSocket(hub *Hub, writer http.ResponseWriter, request *http.Request) {
+func handleClientWebSocket(hub *ws.Hub, writer http.ResponseWriter, request *http.Request) {
 	socket, err := websocket.Upgrade(writer, request, nil, 0, 0)
 	if err != nil {
 		logrus.WithError(err).Warn("Could not upgrade websocket")
@@ -154,7 +77,7 @@ func handleClientWebSocket(hub *Hub, writer http.ResponseWriter, request *http.R
 	hub.HandleConnection(socket)
 }
 
-func handleSenderWebSocket(hub *Hub, writer http.ResponseWriter, request *http.Request) {
+func handleSenderWebSocket(hub *ws.Hub, writer http.ResponseWriter, request *http.Request) {
 	socket, err := websocket.Upgrade(writer, request, nil, 0, 0)
 	if err != nil {
 		logrus.WithError(err).Warn("Could not upgrade websocket")
---
+++ ws/registrar.go
@@ -0,0 +1,85 @@
+package ws
+
+import (
+	"time"
+	"sync"
+)
+
+type Stream struct {
+	Id      string    `json:"id"`
+	Created time.Time `json:"created"`
+
+	hub     *Hub
+}
+
+type Registrar struct {
+	lock    sync.RWMutex
+	streams map[string]*Stream
+}
+
+func NewRegistrar() *Registrar {
+	return &Registrar{
+		lock:    sync.RWMutex{},
+		streams: make(map[string]*Stream),
+	}
+}
+
+func (r *Registrar) GetExistingHub(streamId string) *Hub {
+	r.lock.RLock()
+	defer r.lock.RUnlock()
+
+	stream := r.streams[streamId]
+	if stream == nil {
+		return nil
+	}
+
+	return stream.hub
+}
+
+func (r *Registrar) GetOrCreateHub(streamId string) *Hub {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	stream, _ := r.streams[streamId]
+	if stream == nil {
+		stream = &Stream{
+			Id:      streamId,
+			Created: time.Now(),
+			hub:     NewHub(),
+		}
+
+		r.streams[streamId] = stream
+
+		go stream.hub.MainLoop()
+	}
+
+	return stream.hub
+}
+
+func (r *Registrar) GetStreams() []Stream {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	// get a copy of the streams id.
+	streams := make([]Stream, 0, len(r.streams))
+	for _, stream := range r.streams {
+		streams = append(streams, *stream)
+	}
+
+	return streams
+}
+
+func (r *Registrar) Close(streamId string) bool {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	if stream := r.streams[streamId]; stream != nil {
+		delete(r.streams, streamId)
+		stream.hub.RequestShutdown()
+		return true
+
+	} else {
+		return false
+	}
+}
+
